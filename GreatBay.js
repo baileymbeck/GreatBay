c43ecf7385d9172100613e0bce706a2d3d4fc3f7
Chris Mlinac <chrismlinac@gmail.com>
2019-10-04 13:29:51 -0500

    env

---
+++ GreatBay.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: "root",
+  
+    // Your password
+    password: process.env.DB_PASSWORD,
+    database: "ice_creamDB"
+  });
+  
+
+  connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected");
+    connection.end();
+  });
