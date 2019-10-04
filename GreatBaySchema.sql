da92f2e3a0298725914c671968fad852233dc3c3
erika gonzalez <gonzalezerika75951@gmail.com>
2019-10-04 13:11:10 -0500

    added the basic sql

---
+++ GreatBaySchema.sql
@@ -0,0 +1,12 @@
+DROP DATABASE IF EXIST greatbay_db;
+
+CREATE DATABASE greatbay_db;
+
+USE greatbay_db;
+
+CREATE TABLE greatbay (
+    id INT (10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    item VARCHAR (48) NOT NULL,
+    category VARCHAR (50) NOT NULL,
+    bid INT (12) NOT NULL;
+)
